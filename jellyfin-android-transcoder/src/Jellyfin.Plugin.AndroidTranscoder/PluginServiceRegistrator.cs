f020dec6b4b3d56cee72549c23b4ba172e329517
doctorpangloss <2229300+doctorpangloss@users.noreply.github.com>
2026-06-19 11:24:13 -0700

    fix: clear docker ffmpeg env before startup validation

--- jellyfin-android-transcoder/src/Jellyfin.Plugin.AndroidTranscoder/PluginServiceRegistrator.cs
+++ jellyfin-android-transcoder/src/Jellyfin.Plugin.AndroidTranscoder/PluginServiceRegistrator.cs
@@ -1,4 +1,5 @@
 using MediaBrowser.Controller;
+using MediaBrowser.Controller.Extensions;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +9,7 @@ public sealed class PluginServiceRegistrator : IPluginServiceRegistrator
 {
     public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
     {
+        Environment.SetEnvironmentVariable("JELLYFIN_" + ConfigurationExtensions.FfmpegPathKey.ToUpperInvariant(), null);
         serviceCollection.AddHostedService<StartupShimInstaller>();
     }
 }
